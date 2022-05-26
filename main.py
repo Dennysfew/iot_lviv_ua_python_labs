2b0889bf546cb00ee0ffb9e112b4956ed92fe293
Dennys Izhyck <denysizhuk@gmail.com>
2022-05-26 09:11:47 +0300

    Fixed problem with creating objects

--- main.py
+++ main.py
@@ -1,15 +1,20 @@
-from models import VacuumCleaner, Television, WashingMachine, MusicCenter, TapeRecorder, RadioReceiver
+from models.MusicCenter import MusicCenter
+from models.RadioReceiver import RadioReceiver
+from models.TapeRecorder import TapeRecorder
+from models.Television import Television
+from models.VacuumCleaner import VacuumCleaner
+from models.WashingMachine import WashingMachine
 
-bosh = MusicCenter.MusicCenter("Music center", 400, 25, 1, 40)
+bosh = MusicCenter("Music center", 400, 25, 1, 40)
 print(bosh.get_music_center())
 print(str(bosh))
-samsung = WashingMachine.WashingMachine("Washing machine", 500, 66, 9)
+samsung = WashingMachine("Washing machine", 500, 66, 9)
 print(samsung.get_washing_machine())
-philips = VacuumCleaner.VacuumCleaner("Vacuum cleaner", 70, 4, False)
+philips = VacuumCleaner("Vacuum cleaner", 70, 4, False)
 print(philips.get_vacuum_cleaner())
-kivi = Television.Television("TV", 60, 8, 43)
+kivi = Television("TV", 60, 8, 43)
 print(kivi.get_television())
-giradischi = TapeRecorder.TapeRecorder("Tape recorder", 25, 2, 50)
+giradischi = TapeRecorder("Tape recorder", 25, 2, 50)
 print(giradischi.get_radio_receiver())
-okord = RadioReceiver.RadioReceiver("Radio receiver", 20, 3, 20)
+okord = RadioReceiver("Radio receiver", 20, 3, 20)
 print(okord.get_radio_receiver())
